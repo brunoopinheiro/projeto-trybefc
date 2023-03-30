4e41b4f31780ecd05b6bc9cc74245986c179e1c7
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-03-30 10:19:05 -0300

    feat: migration and model for matches table

---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,63 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import TeamsModel from './TeamModel';
+
+class MatchModel extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+MatchModel.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+TeamsModel.belongsTo(MatchModel, { foreignKey: 'homeTeamId', as: 'id' });
+TeamsModel.belongsTo(MatchModel, { foreignKey: 'awayTeamId', as: 'id' });
+
+export default MatchModel;
